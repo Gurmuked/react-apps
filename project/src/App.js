15b4972bfe094692e675a6ada5252165c530dc9d
Gurmesa <gurmesakedir96@gmail.com>
2025-04-08 16:25:50 +0300

    Form page that has registration and login page

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Calculator from '../src/components/Calculator/calculator.jsx';
-
+import Login from './components/login and registration/login.jsx';
 
 
 function App() {
   return (
     <div>
       <Calculator />
+      <Login />
     </div>
   );
 }
---
+++ project/src/components/login and registration/login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './login.css'
+
+ function login() {
+  return (
+    <div className='wrapper'>
+      <div className="login">
+        <form>
+          <h1>LOGIN</h1>
+          <div className='inputBox'>
+            <input type="text"
+                     placeholder="Username" className='inputclass'/>
+          </div>
+          <div className='inputBox'>
+            <input className='inputclass' type="password"
+                     placeholder="password" />
+          </div>
+          
+
+          <button type='submit'className='submit'>Login</button>
+          <div className='rember forgot'>
+            <label><input type="checkbox"
+                    />Remember Me</label>
+                    <a href="#">  forgot password</a>
+          </div>
+          <div className='forgot'>
+            <p>Don't have an account?
+              <a href='#'>Register</a>
+            </p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  )
+}
+export default login;
