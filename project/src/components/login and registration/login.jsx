a373b62d3a69cef9dcbadf05d022854ea6c8470f
Gurmesa <gurmesakedir96@gmail.com>
2025-04-15 23:59:42 +0300

    Registeration page

--- project/src/components/login and registration/login.jsx
+++ project/src/components/login and registration/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaUser, FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import './login.css'
 
  function login() {
@@ -29,6 +30,41 @@ import './login.css'
           <div className='Register-link'>
             <p>Don't have an account?
               <a href='#'>Register</a>
+            </p>
+          </div>
+
+        </form>
+      </div>
+
+
+      <div className="form-box Register">
+        <form>
+          <h1>REGISTER</h1>
+          <div className='inputBox'>
+            <input type="text"
+                     placeholder="Username" />
+                     <FaUser className='icons'/>
+          </div>
+          <div className='inputBox'>
+            <input type="email"
+                     placeholder="email" />
+                     <MdEmail className='icons'/>
+          </div>
+          <div className='inputBox'>
+            <input  type="password"
+                     placeholder="password" />
+                     <FaLock className='icons'/>
+          </div>
+          <div className='remember-forgot'>
+            <label><input type="checkbox"
+                    />Agree trems and conditions</label>
+          </div>
+
+          <button type='submit'>Register</button>
+        
+          <div className='Register-link'>
+            <p>I Already have an account.
+              <a href='#'>Login</a>
             </p>
           </div>
 
